923c20c612795a84b23ab59d1c7b95d87b8eb367
Thomas <fritschy.thomas@adapei72.asso.fr>
2023-02-14 15:40:43 +0100

    feat(Home): Ajout du bouton de création de discussion sur la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,26 @@ import { PlusIcon, QueueListIcon } from "@heroicons/react/24/outline";
 import SignOut from "../components/SignOut";
 import Hello from "../components/Hello";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { addDoc, collection, doc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
 
 const HomePage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const docRef = await addDoc(
+      collection(doc(db, "users", session?.user?.email!), "chats"),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+    router.push(`/chat/${docRef.id}`);
+  };
+
   return (
     <div className="container relative flex flex-col items-center justify-center h-screen mx-auto text-neutral-800 ">
       <Image src="/images/illustration.svg" alt="" width={400} height={400} />
@@ -18,7 +36,10 @@ const HomePage = () => {
       <Hello />
       <div className="absolute w-full bottom-6 lg:bottom-12 lg:px-[30%] left-0 p-4 px-12 flex items-center justify-between">
         <SignOut />
-        <button className="p-2 border-2 rounded-2xl border-neutral-300 animate-bounce">
+        <button
+          onClick={createNewChat}
+          className="p-2 border-2 rounded-2xl border-neutral-300 animate-bounce"
+        >
           <PlusIcon className="w-10 h-10" />
         </button>
         <Link href="/chat">
